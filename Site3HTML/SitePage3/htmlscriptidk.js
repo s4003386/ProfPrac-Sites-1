b96074e55f1ea645b8d883777663330f9f8f16b5
Wolfram <130018275+s4003386@users.noreply.github.com>
2025-06-11 21:47:00 +1000

    github update

--- Site3HTML/SitePage3/htmlscriptidk.js
+++ Site3HTML/SitePage3/htmlscriptidk.js
@@ -14,3 +14,23 @@ setInterval(() => {
   console.log('set', images[idx]);
   target.style.backgroundImage = `url('${images[idx]}')`;
 }, 1000); 
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const neoImages = [
+        "/Site3HTML/SitePage3/NeoEmote/neoemote1.png",
+        "/Site3HTML/SitePage3/NeoEmote/neoemote2.png",
+        "/Site3HTML/SitePage3/NeoEmote/neoemote3.png",
+        "/Site3HTML/SitePage3/NeoEmote/neoemote4.png",
+        "/Site3HTML/SitePage3/NeoEmote/neoemote5.png",
+    ];
+    let current = 0;
+    const neoImg = document.querySelector('.NeoImage');
+    if (!neoImg) return;
+
+    neoImg.addEventListener('click', function() {
+        current = (current + 1) % neoImages.length;
+        neoImg.src = neoImages[current];
+    });
+});
